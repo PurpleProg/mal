c245050042302a3cf656e79f5742f1e29ca3d5a5
marcel <marcel.phippen@proton.me>
2025-01-17 03:31:21 +0100

    use getline for step0

--- impls/c.3/step0_repl.c
+++ impls/c.3/step0_repl.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
 
 
-char* READ(char string[]);
-char* EVAL(char string[]);
-char* PRINT(char string[]);
-char* rep(char string[]);
+char* READ(char * line);
+char* EVAL(char * line);
+char* PRINT(char * line);
+char* rep(char * line);
 
 
-int main(void)
-{
-    char input[255];
+int main(void) {
+    char * line = NULL;
+    size_t len = 0;
 
-    do
-    {
-        printf("%s", rep(input));
+    printf("user> ");
+    while (getline(&line, &len, stdin) != -1) {
+        printf("%s", rep(line));
         printf("user> ");
-    } while (fgets(input, 255, stdin));
+    }
 }
 
 
-char* READ(char string[])
-{
-    return string;
+char* READ(char * line) {
+    return line;
 }
 
 
-char* EVAL(char string[])
-{
-    return string;
+char* EVAL(char * line) {
+    return line;
 }
 
 
-char* PRINT(char string[])
-{
-    return string;
+char* PRINT(char * line) {
+    return line;
 }
 
 
-char* rep(char string[])
-{
-    return PRINT(EVAL(READ(string)));
+char* rep(char * line) {
+    return PRINT(EVAL(READ(line)));
 }
